ce1ccfdbc5267058753048d8851965dd826df0f6
rayen-developer <rayendeveloper@gmail.com>
2026-02-12 13:24:54 +0100

    add Product class

---
+++ src/main/java/com/haiilo/checkout/domain/Product.java
@@ -0,0 +1,15 @@
+package com.haiilo.checkout.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Product {
+    private String name;
+    private BigDecimal price;
+}
