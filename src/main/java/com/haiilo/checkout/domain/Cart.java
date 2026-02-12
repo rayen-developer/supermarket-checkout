ead1502b2e6078b5d0e0ddd2c881428300ed3a45
rayen-developer <rayendeveloper@gmail.com>
2026-02-12 14:44:29 +0100

    add Cart class

---
+++ src/main/java/com/haiilo/checkout/domain/Cart.java
@@ -0,0 +1,34 @@
+package com.haiilo.checkout.domain;
+
+import lombok.Getter;
+import java.util.HashMap;
+import java.util.Map;
+import java.math.BigDecimal;
+
+@Getter
+public class Cart {
+    // key: product name, value: CartItem
+    private final Map<String, CartItem> items = new HashMap<>();
+
+    // Add product with quantity
+    public void addProduct(Product product, int quantity) {
+        if (items.containsKey(product.getName())) {
+            CartItem current = items.get(product.getName());
+            current.setQuantity(current.getQuantity() + quantity);
+        } else {
+            items.put(product.getName(), new CartItem(product, quantity));
+        }
+    }
+
+    // Remove a product by name
+    public void removeProduct(String productName) {
+        items.remove(productName);
+    }
+
+    // Get total price for this cart
+    public BigDecimal getSubtotal() {
+        return items.values().stream()
+                .map(CartItem::getTotalPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+}
